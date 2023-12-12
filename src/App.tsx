640329dd0025f276471950b0935f27478b1db145
Christian <christian.tellez.giraldo@gmail.com>
2023-12-12 13:50:00 +0100

    chore: small changes to app and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import { AddTaskForm } from "./components/add-task-form";
 import { StartupProgress } from "./components/startup-progress";
-import { STAGE_NAMES, type StageType } from "./components/startup-progress.types";
+import {
+  STAGE_NAMES,
+  type StageType,
+} from "./components/startup-progress.types";
 
 const stages: StageType[] = [
   { id: 1, name: STAGE_NAMES.FOUNDATION, tasks: [] },
@@ -9,13 +12,13 @@ const stages: StageType[] = [
   { id: 3, name: STAGE_NAMES.DELIVERY, tasks: [] },
 ];
 
-function App() {
+const App = () => {
   return (
     <>
       <AddTaskForm />
       <StartupProgress stages={stages} />
     </>
   );
-}
+};
 
-export default App;
+export { App };
